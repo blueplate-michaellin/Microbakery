6ceeaccdae0a80460a4c23664255b7c671f69b87
blueplate-michaellin <michael.lin@blueplate.co>
2019-02-25 23:08:00 +0800

    add google analytics package

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,11 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
+      {
+        resolve: `gatsby-plugin-google-analytics`,
+        options: {
+          trackingId: "UA-135120897-1",
+      },
     }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
