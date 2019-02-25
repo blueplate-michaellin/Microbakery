9a096a2e462dd6dc1c4c4701f93c532823e69e11
blueplate-michaellin <michael.lin@blueplate.co>
2019-02-25 23:02:12 +0800

    Added tag for google analytics

--- src/components/head.js
+++ src/components/head.js
@@ -3,6 +3,16 @@ import { Helmet } from "react-helmet"
 
 const Head = () => (
     <Helmet>
+      {/** Global site tag (gtag.js) - Google Analytics **/}
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-135120897-1"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-135120897-1');
+      </script>
+
       {/** UIkit CSS **/}
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/css/uikit.min.css" />
 
