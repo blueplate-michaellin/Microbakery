43d3b004247e14c1e009f28f44825449850265da
blueplate-michaellin <michael.lin@blueplate.co>
2019-03-06 20:21:31 +0800

    configed meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Microbakery`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `We sell the healthist and most nutricious sourdough bread in Singapore.`,
     author: `Michael Lin`,
     siteUrl: `https://www.microbakery.co`,
   },
